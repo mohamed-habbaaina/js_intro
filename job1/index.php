7a7486040d36eefce88ce387501491df687ee315
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-01-17 08:39:55 +0100

    getelementyId().innerHTML & document.write()

---
+++ job1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job1</title>
+</head>
+<body>
+<h2>My First Web Page</h2>
+<p>My First Paragraph.</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = 5 + 6 + ' Hello';
+</script>
+ 
+</body>
+</html>
