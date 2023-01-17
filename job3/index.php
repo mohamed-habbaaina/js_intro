2ef98ef8356139c9255a1312c9c760754f712589
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-01-17 08:41:23 +0100

    onclick & window.print()

---
+++ job3/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job3</title>
+</head>
+<body>
+<h2>My First Web Page</h2>
+<p>My First Paragraph.</p>
+
+<button type="button" onclick="document.write(5 + 6)">Try it</button>
+ 
+</body>
+</html>
---
+++ job4/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job4</title>
+</head>
+<body>
+<h2>My First Web Page</h2>
+<p>Pour imprimer la page</p>
+
+<button type="button" onclick="window.print()">Print</button>
+ 
+</body>
+</html>
