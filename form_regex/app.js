e6f659eb58bfb95d0a2f9f6073d9a7a520057143
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-02-02 16:11:54 +0100

    Validation de Password RegExp

--- form_regex/app.js
+++ form_regex/app.js
@@ -1,7 +1,20 @@
+// ********* Ecouter les Evenements *********
+
 let form = document.querySelector('#loginForm');
+
+// Ecouter change Email
+// form.email return input email
 form.email.addEventListener('change', function(){
     validEmail(this);
 });
+
+// Ecouter change Password
+form.password.addEventListener('change', function(){
+    validPassword(this);
+})
+
+
+// ********* Validation Email *********
 const validEmail = function(inputEmail) {
 
     // Création de la Regexp pour valider l'email
@@ -18,8 +31,52 @@ const validEmail = function(inputEmail) {
     if(testEmail){
         small.innerHTML = 'Adresse Email Valide';
         small.style.color = 'green';
+        return true; // pour l'utiliser dans la function de submit form
     } else{
         small.innerHTML = 'Adresse Email Pas Valide (Carectère Valide: A-Z 0-9 _.-)';
         small.style.color = 'red';
+        return false;
     }
 };
+
+// ********* Validation Password *********
+const validPassword = function(inputPassword){
+    let msg;
+    let valid = false;
+
+    // verifier que Password contien minimum 4 caracteres;
+    if(inputPassword.value.length < 4){
+        msg = 'Password trop cours Minimum 4 caracteres !';
+    }
+
+    //  Au moins une maj
+    else if (!/[A-Z]/.test(inputPassword.value)){
+        msg = 'Le Password dois contenir au moins une Majuscule !';
+    }
+
+    //  Au moins une min
+    else if (!/[a-z]/.test(inputPassword.value)){
+        msg = 'Le Password dois contenir au moins une Miniscule !';
+    }
+
+    //  Au moins un chiffre
+    else if (!/[0-9]/.test(inputPassword.value)){
+        msg = 'Le Password dois contenir au moins un Chiffre !';
+    }
+
+    else{
+        msg = 'Le Password est Valide';
+        valid = true;
+    }
+    
+    // L'Affichage de la validation de Password.
+    let smallPass = inputPassword.nextElementSibling;
+
+    if(valid){
+        smallPass.innerHTML = 'Password Valide';
+        smallPass.style.color = 'green';
+    } else{
+        smallPass.innerHTML = msg;
+        smallPass.style.color = 'red';
+    }    
+}
